27a9e3473b537d6e5a3d77bb01db1d7af698007a
margobardonova <bardonovamargo@gmail.com>
2022-08-14 18:26:00 +0300

    init commit

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import s from './Header.module.css'
+import {NavLink} from 'react-router-dom';
+import {PATH} from './Pages';
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <NavLink to={PATH.PRE_JUNIOR} className={({ isActive }) => (isActive ? 'active' : s.inactive)} >pre-junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={({ isActive }) => (isActive ? 'active' : s.inactive)}>junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS} className={({ isActive }) => (isActive ? 'active' : s.inactive)}>junior+</NavLink>
+            <div className={s.block}/>
         </div>
-    )
+    );
 }
 
 export default Header
--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PreJunior from './pages/PreJunior';
+import Error404 from './pages/Error404';
+import {Routes, Route, Navigate} from 'react-router-dom';
+import Junior from './pages/Junior';
+import JuniorPlus from './pages/JuniorPlus';
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
+    JUNIOR: '/junior',
+    JUNIOR_PLUS:'/junior-plus'
     // add paths
 }
 
@@ -9,20 +16,22 @@ function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
-            {/*<Routes>*/}
+            <Routes>
 
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                {/*<Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>*/}
-
-                {/*<Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>*/}
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
                 // add routes
 
-                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                {/*<Route path={'/*'} element={<Error404/>}/>*/}
+                <Route path={PATH.JUNIOR} element={<Junior/>}/>
+                <Route path={PATH.JUNIOR_PLUS} element={<JuniorPlus/>}/>
 
-            {/*</Routes>*/}
+                {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
+                <Route path={'/*'} element={<Error404/>}/>
+
+            </Routes>
         </div>
-    )
+    );
 }
 
 export default Pages
